5536fe28e623155bd90dc09c711bb7b1df526c14
Ruari Day <ruariday@gmail.com>
2019-12-03 17:17:28 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,7 +38,7 @@ end
 
 def matrix_update(matrix, row, column, new_value)
   array_literal_matrix[0][2] = "Tiny Ewell"
-  new_value = array_literal_matrix[0]
+  new_value = array_literal_matrix
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
 end
