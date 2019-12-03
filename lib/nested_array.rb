564621017a95ede7962795e95b2baab760b41ba4
Ruari Day <ruariday@gmail.com>
2019-12-03 17:31:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,9 +37,6 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  ENNET_HOUSE
-  ENNET_HOUSE[2] = "Tiny Ewell"
-  ENNET_HOUSE[0,1,2,3,4]
 # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
 end
